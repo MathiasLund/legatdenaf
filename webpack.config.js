70294e26e5ddc56d1aa33340bc600ddf3cda9400
Mathias Lund <mathias@graduateland.com>
2016-09-25 20:12:52 +0200

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+ entry: './app/client.js',
+ output: { path: __dirname + '/public/js/build', filename: 'main.build.js' },
+ module: {
+   loaders: [
+   {
+     test: /.jsx?$/,
+     loader: 'babel',
+     exclude: /node_modules/,
+     query: {presets: ['react', 'es2015']}
+   }]
+ },
+};

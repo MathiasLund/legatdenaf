6f71e32273ac52800ddfdd3b6ed3cc694f1988ed
Mathias Lund <mathias@graduateland.com>
2016-09-25 23:16:34 +0200

    working react

--- app/server.js
+++ app/server.js
@@ -1,19 +1,19 @@
 import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import App from './components/App'
 
 var app = express.Router();
 
 app.get('/', function(req, res, next) {
 
-  let element = React.createElement('div', null, 'Hello World!');
-  let component = renderToString(element);
+  let component = renderToString(
+      <App></App>
+  );
 
-  res.render('index', {
-    component: component,
-    title: 'Festivalfinder'
-  });
-
+  res.send(
+    component
+  )
 });
 
 module.exports = app;

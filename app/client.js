a53d9cde06ce9f6ad298397b43f3e74081671682
Mathias Lund <mathias@graduateland.com>
2016-10-22 21:03:58 +0200

    split out components

--- app/client.js
+++ app/client.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
+import App from './components/App'
 import SpotifyBtn from './components/Button'
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>festivalfinder</h1>
-        <div>{this.props.children}</div>
-      </div>
-    )
-  }
-}
 
 class Home extends React.Component {
   render() {
@@ -21,16 +11,6 @@ class Home extends React.Component {
         <h1>Home</h1>
         <SpotifyBtn />
         <Link to="/about">Link</Link>
-      </div>
-    )
-  }
-}
-
-class About extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>About</h1>
       </div>
     )
   }
--- app/components/App/index.js
+++ app/components/App/index.js
@@ -3,18 +3,9 @@ import React, { Component } from 'react'
 export default class App extends Component {
     render() {
         return (
-          <html>
-            <head>
-              <meta name="viewport" content="width=device-width, initial-scale=1"/>
-              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
-              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
-            </head>
-            <body>
-              <div id='wrapper'>
-                {this.props.children}
-              </div>
-            </body>
-          </html>
+          <div>
+            <div>{this.props.children}</div>
+          </div>
         )
     }
 }

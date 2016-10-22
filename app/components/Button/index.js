a61a7ed995c74c1fb0a692342c1bd622a4e38c28
Graduateland <Graduateland@Graduatelands-iMac.local>
2016-10-22 12:28:08 +0200

    initial homepage template work

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -4,7 +4,44 @@ import { Button } from 'react-bootstrap';
 export default class SpotifyBtn extends Component {
     render() {
         return (
-          <Button href="/spotify/login" bsStyle="success"><i className="fa fa-spotify"></i> Login with Spotify</Button>
+          <div className="container">
+            <div className="row header">
+              <div className="logo pull-left">
+                Logo
+              </div>
+              <div className="logo pull-right">
+                Browse Login
+              </div>
+            </div>
+            <div className="row main-body">
+              <div className="col-md-8 col-md-offset-2">
+                <div className="h1 text-white text-center">
+                  Find the perfect festival.
+                  <br/>
+                  Scan your music.
+                </div>
+                <div className="p text-white text-center">
+                  Plug in your music streaming app and we’ll do all the work
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div clasName="row cta-group">
+                  <div className="col-sm-3 col-sm-offset-3">
+                    <Button className="btn btn-lg" href="/spotify/login" bsStyle="success">
+                      <i className="fa fa-spotify"></i>
+                      Login with Spotify
+                    </Button>
+                  </div>
+                  <div className="col-sm-3">
+                    <Button className="btn btn-lg" href="/spotify/login" bsStyle="success">
+                      <i className="fa fa-spotify"></i>
+                      Login with Apple Music
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         )
     }
 }

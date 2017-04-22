7559b9bea8f09c521af8ace65645fe5f27fe2af4
Mathias Lund <mathias@graduateland.com>
2017-04-22 10:19:12 +0200

    working

--- app/client.js
+++ app/client.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
 import App from './components/App'
-import About from './components/About'
 import Homepage from './components/Homepage'
 
 ReactDOM.render(
   <Router history={browserHistory}>
   <Route path="/" component={App}>
     <IndexRoute component={Homepage} />
-    <Route path="about" component={About} />
   </Route>
 </Router>,document.getElementById("app"))
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express')
 var app = express()
 var routes = require('./app/server');
-var spotifyRoutes = require('./app/spotify.js');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(spotifyRoutes);
 app.use(routes);
 
 const PORT = 3000

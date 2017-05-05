dac3522ddf083050d503cd46281d290f07097d0c
Mathias Lund <mathias@graduateland.com>
2017-05-05 09:17:09 +0200

    progress bar

--- app/components/ProgressBar/index.js
+++ app/components/ProgressBar/index.js
@@ -6,12 +6,29 @@ export default class ProgressBar extends Component {
     render() {
 
       let step = this.props.step;
+      let val;
+      let title;
+
+      switch (step) {
+        case "step1":
+          val = 33
+          title = "1/3: Vælg uddannelsesinstitution"
+          break;
+        case "step2":
+          val = 66
+          title = "2/3: Vælg uddannelse"
+        case "step3":
+          val = 100
+          title = "3/3: Vælg udvekslingsdestination"
+        default:
+
+      }
 
         return (
           <div className="progress">
-            <div className={`progress-bar progress-bar-success ${step}`} role="progressbar" aria-valuenow="33"
+            <div className={`progress-bar progress-bar-success ${step}`} role="progressbar" aria-valuenow={`${val}`}
             aria-valuemin="0" aria-valuemax="100">
-              33% complete
+              {title}
             </div>
           </div>
         )
